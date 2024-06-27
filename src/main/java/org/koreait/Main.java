60da33f34da151293e26042e573a609985a51d84
JerryKiki <gasi758@gmail.com>
2024-06-27 14:09:06 +0900

    Q1, Q2

--- src/main/java/org/koreait/Main.java
+++ src/main/java/org/koreait/Main.java
@@ -9,10 +9,11 @@ public class Main {
 
         //문제 1번
         System.out.println("==문제 1번 실행==");
+        System.out.println("입력하신 가로길이, 세로길이에 따라 사각형 모양으로 별을 찍습니다.");
 
         while (true) {
             try {
-                System.out.print("가로길이, 세로길이를 공백을 사이에 두고 입력해주세요 : ");
+                System.out.print("가로길이, 세로길이를 공백 혹은 엔터로 구분하여 입력해주세요 : ");
                 int q1Width = sc.nextInt();
                 int q1Height = sc.nextInt();
                 Question1.solution(q1Width, q1Height);
@@ -27,5 +28,23 @@ public class Main {
         //문제 2번
         System.out.println();
         System.out.println("==문제 2번 실행==");
+        System.out.println("입력하신 양의 정수값에 따라 숫자 패턴의 피라미드를 출력합니다.");
+        while (true) {
+            try {
+                System.out.print("양의 정수를 입력해주세요 : ");
+                int num = sc.nextInt();
+                Question2.Solution(num);
+                sc.nextLine();
+                break;
+            } catch (InputMismatchException e) {
+                System.out.println("올바른 입력값이 아닙니다.");
+                sc.nextLine();
+            }
+        }
+
+//        //문제 3번
+//        System.out.println();
+//        System.out.println("==문제 3번 실행==");
+
     }
 }
--- src/main/java/org/koreait/Question2.java
+++ src/main/java/org/koreait/Question2.java
@@ -1,5 +1,25 @@
 package org.koreait;
 
-public class Question2 {
+import java.util.InputMismatchException;
 
+// 양의 정수 n을 요청받아 숫자 패턴의 피라미드를 출력하는 프로그램을 구현합니다.
+// 피라미드는 n 레벨을 가져야 하며, 최상위 레벨에서 n부터 시작하여 맨 아래층에서 1까지 숫자가 감소해야 합니다. (30점)
+// for문 혹은 while문 둘다 사용가능
+// Scanner 활용
+// 중첩반복문을 활용
+public class Question2 {
+    static void Solution(int num) {
+
+        if (num <= 0) throw new InputMismatchException();
+
+        for (int i = 0; i < num; i++) { //줄 수
+            for (int j = 0; j < i; j++) { //공백 출력
+                System.out.print(" ");
+            }
+            for (int j = num - i; j > 0; j--) { //숫자 출력
+                System.out.printf("%d ", j);
+            }
+            System.out.println();
+        }
+    }
 }
